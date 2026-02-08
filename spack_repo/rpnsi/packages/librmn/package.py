4bdac40ef7cdfacb6f0a00271c62a10d46a2ba8a
Philippe Carphin <pcarphin@mac.home>
2026-02-07 23:09:53 -0500

    Spack repo for librmn
    spack repo add git@github.com:philippecarphin/rpn-spack-repos
    spacktivate <AN_ENVIRONMENT>
    spack add librmn@WIP-m2-build -mpi build_type=Debug
    spack install librmn

---
+++ spack_repo/rpnsi/packages/librmn/package.py
@@ -0,0 +1,54 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+# ----------------------------------------------------------------------------
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install librmn
+#
+# You can edit this file again by typing:
+#
+#     spack edit librmn
+#
+# See the Spack documentation for more information on packaging.
+# ----------------------------------------------------------------------------
+
+from spack_repo.builtin.build_systems.cmake import CMakePackage
+
+from spack.package import *
+
+
+class Librmn(CMakePackage):
+    """A library of functions for numerical weather prediction used
+    primarily by Environment and and Climate Change Canada.
+
+    Its main components are Standard RPN files and the EZ interpolator."""
+
+    homepage = "https://github.com/ECCC-ASTD-MRD/librmn"
+    git = homepage
+    url = "librmn"
+
+    maintainers("rpn-si")
+
+    license("LGPL-2.1-only", checked_by="pc")
+
+    version("master", branch="master", submodules=True)
+    version("dev_alpha", branch="dev_alpha", submodules=True)
+    version("WIP-m2-build", branch="WIP-m2-build", submodules=True)
+
+    variant("mpi", default=False)
+
+    with when("+mpi"):
+        depends_on("openmpi")
+
+    def cmake_args(self):
+        args = []
+        with when("-mpi"):
+            args.append("-DWITH_OMPI=OFF")
+        return args
