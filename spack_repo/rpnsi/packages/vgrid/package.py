0b1db7a474282188c1d53684dbc51c600d236704
Philippe Carphin <phil103@hotmail.com>
2026-02-08 11:30:52 -0500

    WIP: Start vgrid package
    spack add vgrid to empty environment, then spack install builds and installs
    cmake_rpn, librmn, and tdpack and starts to build vgrid but fails during cmake
    because find_package(rmn ${rmn_REQ_VERSION} REQUIRED) wants version 20.0.0 which
    I don't have.

---
+++ spack_repo/rpnsi/packages/vgrid/package.py
@@ -0,0 +1,44 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+# ----------------------------------------------------------------------------
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install vgrid
+#
+# You can edit this file again by typing:
+#
+#     spack edit vgrid
+#
+# See the Spack documentation for more information on packaging.
+# ----------------------------------------------------------------------------
+
+from spack_repo.builtin.build_systems.cmake import CMakePackage
+
+from spack.package import *
+
+class Vgrid(CMakePackage):
+    """It's vgrid, what can I say"""
+
+    homepage = "https://github.com/ECCC-ASTD-MRD/vgrid"
+    git = homepage
+
+    maintainers("rpn-si")
+
+    license("LGPL-2.1-only", checked_by="pc")
+
+    variant("tdpack", default=True)
+
+    version("master", branch="master")
+    version("dev", branch="dev")
+
+    depends_on("cmake-rpn", type="build")
+    depends_on("librmn", type="build")
+    depends_on("tdpack", type="build", when="+tdpack")
+
